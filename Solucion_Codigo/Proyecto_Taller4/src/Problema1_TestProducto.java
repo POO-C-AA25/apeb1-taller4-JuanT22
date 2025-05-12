ab51aadde71c05c8e40ee9be9bf335fc95bd3337
Juan Tacuri <tacurijuanito28@gmail.com>
2025-05-11 22:12:52 -0500

    new

---
+++ Solucion_Codigo/Proyecto_Taller4/src/Problema1_TestProducto.java
@@ -0,0 +1,39 @@
+public class Problema1_TestProducto {
+    public static void main(String[] args) {
+        Producto producto1 = new Producto(100, 10); 
+        producto1.calcularDescuento(producto1.precioP, producto1.cantidadP);
+        producto1.calcularPrecioFinal();
+        System.out.println(producto1);
+    }
+}
+
+class Producto{
+    public double precioP;
+    public int    cantidadP;
+    public double descuento;
+    public double precioFinal;
+    public double montoD;
+
+    public Producto() {
+    }
+
+    public Producto(double precioP, int cantidadP) {
+        this.precioP = precioP;
+        this.cantidadP = cantidadP;
+    }
+    public double calcularDescuento(double precioP, int cantidadP){
+        this.descuento = (this.precioP >= 1000 && this.cantidadP >= 10) ? (10) : (5) ;
+        this.montoD = (this.cantidadP * this.precioP) * (this.descuento / 100); 
+        return this.montoD;
+    }
+    public double calcularPrecioFinal(){
+        this.precioFinal = (this.cantidadP * this.precioP) - this.montoD;
+        return this.precioFinal;
+    }
+        
+    @Override
+    public String toString() {
+        return "Producto{" + "precioP=" + precioP + ", cantidadP=" + cantidadP + ", descuento=" + descuento + ", precioFinal=" + precioFinal + ", montoD=" + montoD + '}';
+    }
+    
+}
